5eb23c6c73039e25cbd0db4e0d7d0a00dd9d0337
isma <ismaelbies@hotmail.com>
2021-09-03 16:53:24 -0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,28 @@ public class Main {
 
     static final Map<String, Integer> instrucoesLookup = new HashMap<String, Integer>() {{
         // (instrucao, latencia em ciclos)
-        put("100000", null); //	add
-        put("100001", null); //	addu
-        put("001000", null); //	addi
-        put("001001", null); //	addiu	
-        put("100100", null); //	and	
-        put("001100", null); //	andi
-        put("011010", null); //	div	
-        put("011011", null); //	divu
-        put("011000", null); //	mult
-        put("011001", null); //	multu
-        put("100111", null); //	nor
-        put("100101", null); //	or
+        put("100000", 4); //	add
+        put("100001", 4); //	addu
+        put("001000", 4); //	addi
+        put("001001", 3); //	addiu
+        put("100100", 4); //	and
+        put("001100", 4); //	andi
+        put("011010", 4); //	div
+        put("011011", 4); //	divu
+        put("011000", 4); //	mult
+        put("011001", 4); //	multu
+        put("100111", 4); //	nor
+        put("100101", 4); //	or
         put("001101", null); //	ori
         put("000000", 4); //	sll
-        put("000100", null); //	sllv
-        put("000011", null); //	sra
-        put("000111", null); //	srav
-        put("000010", null); //	srl
+        put("000100", 4); //	sllv
+        put("000011", 4); //	sra
+        put("000111", 4); //	srav
+        put("000010", 4); //	srl
         put("000110", null); //	srlv
-        put("100010", null); //	sub
-        put("100011", null); //	subu
-        put("100110", null); //	xor
+        put("100010", 3); //	sub
+        put("100011", 3); //	subu
+        put("100110", 4); //	xor
         put("001110", null); //	xori
         put("011001", null); //	lhi	
         put("011000", null); //	llo	
@@ -45,7 +45,7 @@ public class Main {
         put("000010", 3); // j
         put("000011", 3); // jal
         put("001001", null); // jalr
-        put("001000", null); // jr
+        put("001000", 4); // jr
         put("100000", null); // lb
         put("100100", null); // lbu
         put("100001", null); // lh
@@ -55,11 +55,12 @@ public class Main {
         put("101000", null); // sb
         put("101001", null); // sh
         put("101011", 4); // sw
-        put("010000", null); // mfhi
-        put("010010", null); // mflo
-        put("010001", null); // mthi
+        put("010000", 4); // mfhi
+        put("010010", 4); // mflo
+        put("010001", 4); // mthi
         put("010011", null); // mtlo
         put("011010", null); // trap
+        put("001111", null);  // lui
     }};
 
     final static int MAX_CICLOS = 5;
@@ -88,6 +89,10 @@ public class Main {
             while ((linha = br.readLine()) != null) {
                 System.out.printf("Parseando linha %d: %s\n", i, linha);
                 instrucaoEmBits = linha.substring(0, 6);
+
+                if(instrucaoEmBits.equals("000000")) {
+                    instrucaoEmBits = linha.substring(linha.length() -6, linha.length());
+                }
 
                 ciclosDaInstrucao = instrucoesLookup.get(instrucaoEmBits);
                 if (ciclosDaInstrucao == null) {
